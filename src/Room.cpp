22d16c57aa9e5516a67d27ded9af224d02452b3a
OfirHelerman <ofirhelerman9@gmail.com>
2023-04-19 17:28:34 +0300

    Added Room class

---
+++ src/Room.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ofirhelerman on 4/19/23.
+//
+
+#include "Room.h"
+
+
+std::pair<double, double> Room::getEntryPoint() const { return this->entryPoint; }
+int Room::getRoomId() const { return this->roomId; }
+

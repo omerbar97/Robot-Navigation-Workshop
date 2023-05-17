8de767f0e2161672d28bc9322414b79a1e98cb02
omerbar97 <omerbar450@gmail.com>
2023-05-17 13:55:59 +0300

    finished robotWrapper

--- src/Robot/RobotWrapper.cpp
+++ src/Robot/RobotWrapper.cpp
@@ -3,3 +3,51 @@
 //
 
 #include "RobotWrapper.h"
+
+RobotWrapper::RobotWrapper(std::string robotIp, int robotPort, int groundSpeed, int turnSpeed) :
+         robot(robotIp, robotPort) {
+    this->initRobot(robotIp, robotPort);
+    this->robotGroundSpeed = groundSpeed;
+    this->robotTurnSpeed = turnSpeed;
+}
+
+RobotWrapper::~RobotWrapper() {
+    if(this->positionProxy != nullptr) {
+        delete this->positionProxy;
+    }
+    if(this->laserProxy != nullptr) {
+        delete this->laserProxy;
+    }
+}
+
+void RobotWrapper::setRobotPath(std::pair<double, double> path) {
+    this->robotCurrentPath = path;
+}
+
+void RobotWrapper::setRobotSpeed(int speed) {
+    this->robotGroundSpeed = speed;
+}
+
+void RobotWrapper::setRobotTurnSpeed(int speed) {
+    this->robotTurnSpeed = speed;
+}
+
+PlayerCc::Position2dProxy* RobotWrapper::getPos() {
+    return this->positionProxy;
+}
+
+PlayerCc::LaserProxy* RobotWrapper::getLaser() {
+    return this->laserProxy;
+}
+
+PlayerCc::PlayerClient &RobotWrapper::getClient() {
+    return this->robot;
+}
+
+void RobotWrapper::initRobot(std::string robotIp, int robotPort) {
+    std::cout << "connection to robot: " << robotIp << ":" << robotPort << std::endl;
+    this->positionProxy = new PlayerCc::Position2dProxy(&this->robot, 0);
+    std::cout << "connection to robot position proxy" << std::endl;
+    this->laserProxy = new PlayerCc::LaserProxy(&this->robot, 0);
+    std::cout << "connection to robot laser proxy" << std::endl;
+}
--- src/Robot/RobotWrapper.h
+++ src/Robot/RobotWrapper.h
@@ -4,10 +4,39 @@
 
 #ifndef ROBOT_NAVIGATION_WORKSHOP_ROBOTWRAPPER_H
 #define ROBOT_NAVIGATION_WORKSHOP_ROBOTWRAPPER_H
-
+#include <libplayerc++/playerc++.h>
+#include <string>
 
 class RobotWrapper {
-    // pairs of path
+private:
+
+    // robot instance
+    PlayerCc::PlayerClient robot;
+
+    // robot extensions
+    PlayerCc::Position2dProxy* positionProxy;
+    PlayerCc::LaserProxy* laserProxy;
+
+    int robotGroundSpeed;
+    int robotTurnSpeed;
+
+    std::pair<double, double> robotCurrentPath;
+
+    void initRobot(std::string robotIp, int robotPort);
+
+public:
+
+    RobotWrapper(std::string robotIp = "localhost", int robotPort = 6665, int groundSpeed = 0.5, int turnSpeed = 0.1);
+    ~RobotWrapper();
+
+    void setRobotPath(std::pair<double, double> path);
+    void setRobotSpeed(int speed);
+    void setRobotTurnSpeed(int speed);
+
+    // getRobot Extensions
+    PlayerCc::Position2dProxy* getPos();
+    PlayerCc::LaserProxy* getLaser();
+    PlayerCc::PlayerClient& getClient();
 
 };
 

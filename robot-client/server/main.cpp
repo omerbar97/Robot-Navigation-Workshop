d4b9672e3d9eb7258836a46af660206dbadc1434
omerbar97 <omerbar450@gmail.com>
2023-06-10 20:40:42 +0300

    update server

--- robot-client/server/main.cpp
+++ robot-client/server/main.cpp
@@ -4,11 +4,25 @@
 #include "WebSocketServer.h"
 
 int main(int argc, char **argv) {
+    if(argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <ip> <port>" << std::endl;
+        std::cout << "please start again with the following information above!" << std::endl;
+        return 1;
+    }
+
     // server main program
-    WebSocketServer serverWs("localhost",9000);
+    int port = strtol(argv[2], nullptr, 10);
+    if(port < 1024 || port > 65535) {
+        std::cerr << "Error: invalid port number" << std::endl;
+        std::cerr << "make sure the port number is between 1024 to 65535" << std::endl;
+        return 1;
+    }
+
+    WebSocketServer serverWs(argv[1],port);
     serverWs.start();
 
     // Wait for user input to stop the server
+    std::cout << "Server started at " << argv[1] << ":" << port << std::endl;
     std::cout << "Press ENTER to stop the server..." << std::endl;
     std::cin.ignore();
 

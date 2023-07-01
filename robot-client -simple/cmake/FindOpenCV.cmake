85686b9a71120aa714ef515acc75426f5ff9b4bc
Omer Bar <68280043+omerbar97@users.noreply.github.com>
2023-07-01 19:48:01 +0300

    Delete robot-client -simple directory

--- robot-client -simple/cmake/FindOpenCV.cmake
+++
@@ -1,8 +0,0 @@
-## Set the minimum required version of CMake
-#cmake_minimum_required(VERSION 3.24)
-#
-#
-#set(OpenCV_DIR /usr/lib/x86_64-linux-gnu/pkgconfig/)
-#find_package(OpenCV 4 REQUIRED)
-#
-## Report results
